5cc97701833fd1fc7de92824ced3e7f6f7c5dd62
Tramaine Gillus <tramaine.gillus@gmail.com>
2018-11-22 14:22:09 -0500

    Add missing semi-colon.

--- server/utils/message.js
+++ server/utils/message.js
@@ -11,7 +11,7 @@ const generateLocationMessage = (from, { latitude, longitude }) => {
     from,
     url: `https://www.google.com/maps?q=${latitude},${longitude}`,
     createdAt: new Date().getTime()
-  }
+  };
 };
 
 module.exports = { generateMessage, generateLocationMessage };
