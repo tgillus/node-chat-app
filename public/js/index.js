a114c9d180f096ffedd68740d6087af5798e1b71
Tramaine Gillus <tramaine.gillus@gmail.com>
2018-11-24 02:30:01 -0500

    Add CSS for chat page.

--- public/js/index.js
+++ public/js/index.js
@@ -30,13 +30,17 @@ socket.on("newLocationMessage", function(message) {
 jQuery("#message-form").on("submit", function(event) {
   event.preventDefault();
 
+  var messageTextBox = jQuery("[name=message]");
+
   socket.emit(
     "createMessage",
     {
       from: "User",
-      text: jQuery("[name=message]").val()
+      text: messageTextBox.val()
     },
-    function() {}
+    function() {
+      messageTextBox.val("");
+    }
   );
 });
 
@@ -47,12 +51,18 @@ locationButton.on("click", function() {
     return alert("Geolocation not supported by your browser.");
   }
 
+  locationButton.attr("disabled", "disabled").text("Sending Location . . .");
+
   navigator.geolocation.getCurrentPosition(function(position) {
+    locationButton.removeAttr("disabled").text("Send Location");
+
     socket.emit("createLocationMessage", {
       latitude: position.coords.latitude,
       longitude: position.coords.longitude
     });
   }, function() {
+    locationButton.removeAttr("disabled").text("Send Location");
+
     alert("Unable to fetch location.");
   });
 });
